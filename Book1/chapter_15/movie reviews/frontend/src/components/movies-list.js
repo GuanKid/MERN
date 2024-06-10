a15e3059792eb280810666bd60fcb15b65081c94
Desire Gwanzura <112183262+GuanKid@users.noreply.github.com>
2024-06-10 08:46:03 +0200

    Add files via upload

---
+++ Book1/chapter_15/movie reviews/frontend/src/components/movies-list.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react'
+import MovieDataService from "../services/movies"
+import { Link } from "react-router-dom"
+
+const MoviesList = props => {
+
+  const [movies, setMovies] = useState([])
+  const [searchTitle, setSearchTitle] = useState("")
+  const [searchRating, setSearchRating] = useState("")
+  const [ratings, setRatings] = useState(["All Ratings"])
+
+
+  useEffect(() => {
+    retrieveMovies()
+    retrieveRatings()
+  }, [])
+  const retrieveMovies = () => {
+    MovieDataService.getAll()
+      .then(response => {
+        console.log(response.data)
+        setMovies(response.data.movies)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+  const retrieveRatings = () => {
+    MovieDataService.getRatings()
+      .then(response => {
+        console.log(response.data)
+        //start with 'All ratings' if user doesn't specify any ratings
+        setRatings(["All Ratings"].concat(response.data))
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+
+
+  const onChangeSearchTitle = e => {
+    const searchTitle = e.target.value
+    setSearchTitle(searchTitle);
+  }
+  const onChangeSearchRating = e => {
+    const searchRating = e.target.value
+    setSearchRating(searchRating);
+  }
+
+  return (
+    <div className="App">
+      <Container>
+        <Form>
+          <Row>
+            <Col>
+              <Form.Group>
+                <Form.Control
+                  type="text"
+                  placeholder="Search by title"
+                  value={searchTitle}
+                  onChange={onChangeSearchTitle}
+                />
+              </Form.Group>
+              <Button
+                variant="primary"
+                type="button"
+                onClick={findByTitle}
+              >
+                Search
+              </Button>
+            </Col>
+            <Col>
+              <Form.Group>
+                <Form.Control
+                  as="select" onChange={onChangeSearchRating} >
+                  {ratings.map(rating => {
+                    return (
+                      <option value={rating}>{rating}</option>
+                    )
+                  })}
+                </Form.Control>
+              </Form.Group>
+              <Button
+                variant="primary"
+                type="button"
+                onClick={findByRating}
+              >
+                Search
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    </div>
+  );
+}
