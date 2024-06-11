39de72c222209a824b299df8958b45f5a68ec697
Desire Gwanzura <112183262+GuanKid@users.noreply.github.com>
2024-06-11 06:54:27 +0300

    Delete Book1 directory

--- Book1/chapter_14/movie-reviews/frontend/src/components/movies-list.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-function MoviesList() {
-  return (
-    <div className="App">
-      Movies List
-    </div>
-  );
-}
-export default MoviesList;
--- Book1/chapter_16/movie_reviews/front_end/src/components/movies-list.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function MoviesList() {
-  return <div className="App">Movies List</div>;
-}
-export default MoviesList;
