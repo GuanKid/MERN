0a4c6f4c5fc950b79ef0a3bc9f4a746c801b910d
Desire Gwanzura <112183262+GuanKid@users.noreply.github.com>
2024-06-24 09:50:31 +0200

    Add files via upload

---
+++ Tutorial_1/Book-Store/backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://desire:04943655@cluster0.mf6mj8r.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
